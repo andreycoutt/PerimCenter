dd1542a901e0c0b271a2b19a590034f6d5faba04
andreycoutt <contato.andreyyy@gmail.com>
2023-05-06 16:48:03 -0300

    HTML E CSS da pagina inicial

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, SobreView, CadastrarEntregaView, ListarEntregasView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='inicio'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
+    path('cadastrar/', CadastrarEntregaView.as_view(), name='cadastrarentrega'),
+    path('listar/', ListarEntregasView.as_view(), name='listarentregas'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,11 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+class SobreView(TemplateView):
+    template_name = 'sobre.html'
+
+class CadastrarEntregaView(TemplateView):
+    template_name = 'cadastrarentrega.html'
+
+class ListarEntregasView(TemplateView):
+    template_name = 'listarentregas.html'
